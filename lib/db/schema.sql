0dbc886be5e6c834d8ddcd6bbdff92b0d559ee41
Allec A <99637574+axe714@users.noreply.github.com>
2022-06-09 13:34:15 -0700

    Update schema.sql
    Fixed schema.sql file (forgot comma)

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE employees (
     employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL
+    role_id INT NOT NULL,
     manager_id INT ON DELETE SET NULL,
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
     FOREIGN KEY (manager_id) REFERENCES manager(manager_id)
