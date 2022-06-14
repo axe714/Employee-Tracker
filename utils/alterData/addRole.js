4f141b10e1a6fe201abe330ca6f084ee2c405647
Axe714 <axearzadon@gmail.com>
2022-06-14 11:34:17 -0700

    Worked on addrole function

--- utils/alterData/addRole.js
+++ utils/alterData/addRole.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
 const db = require("../../config/connection");
 
-
 // const departmentChoice = () => {
 //   db.promise().query(`SELECT * FROM departments`, (err, results) => {
 //     if (err) throw err;
@@ -13,43 +12,62 @@ const db = require("../../config/connection");
 //   })
 // };
 
-const addRolePrompts = [
-  {
-    type: "input",
-    name: "roleName",
-    message: "What is the name of the role you would like to add?",
-  },
-  {
-    type: "input",
-    name: "roleSalary",
-    message: "What is the salary of the role you would like to add?",
-  },
-  {
-    type: "list",
-    name: "roleDepartment",
-    message: "What department does this role belong to?",
-    //TO DO: grab all departments from department table
-    choices: ["Placeholder 1", "Placeholder 2", "Placeholder 3"],
-  },
-];
-
+// const addRolePrompts = [
+//   {
+//     type: "input",
+//     name: "roleName",
+//     message: "What is the name of the role you would like to add?",
+//   },
+//   {
+//     type: "input",
+//     name: "roleSalary",
+//     message: "What is the salary of the role you would like to add?",
+//   },
+//   {
+//     type: "list",
+//     name: "roleDepartment",
+//     message: "What department does this role belong to?",
+//     //TO DO: grab all departments from department table
+//     choices: departmentChoice
+//   },
+// ];
 
 const addRole = (callback) => {
   db.promise()
     .query(`SELECT * FROM departments`)
-    .then(results => {
-      const choices = results[0].map(departments => {
+    .then((results) => {
+      const departmentChoice = results[0].map((departments) => {
         return {
-          department: departments.department_name,
-          id: departments.id,
+          department_name: departments.department_name,
+          value: departments.id,
         };
       });
-      // console.log(choices);
+      console.table(departmentChoice);
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "roleName",
+            message: "What is the name of the role you would like to add?",
+          },
+          {
+            type: "input",
+            name: "roleSalary",
+            message: "What is the salary of the role you would like to add?",
+          },
+          {
+            type: "list",
+            name: "department_id",
+            message: "What department does this role belong to?",
+            //TO DO: grab all departments from department table
+            choices: departmentChoice,
+          },
+        ])
+        .then((res) => {
+          setTimeout(callback, 1000);
+          console.table(res);
+        });
     });
-  inquirer.prompt(addRolePrompts).then((res) => {
-    setTimeout(callback, 1000);
-    console.table(res)
-  });
 };
 
 module.exports = addRole;
